0035e255320a29d93861f3856ee96cf96de6c1b8
jiwen xin <kevinxin@scripps.edu>
2020-05-01 10:16:58 -0700

    add get query fields method

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ID_RESOLVING_APIS = require('./config').ID_RESOLVING_APIS;
  * @param {object} id_dict - An object of id representation
  * @returns - an id_dict updated with primary id choices
  */
-function get_primary_id(semantic_type, id_dict) {
+exports.get_primary_id = (semantic_type, id_dict) => {
     let ranks = ID_RESOLVING_APIS[semantic_type]["id_ranks"];
     let res = {};
     for (let index = 0; index < ranks.length; index++) {
@@ -38,5 +38,18 @@ exports.get_display_message = (semantic_type, id_dict) => {
     return res.replace(/^\s+|\s+$/g, '');
 }
 
-
-exports.get_primary_id = get_primary_id;
+/**
+ * Get the fields to perform API query
+ * @param {string} semantic_type - The semantic type of IDs
+ * @returns - query field
+ */
+exports.get_query_fields = (semantic_type) => {
+    let mapping = ID_RESOLVING_APIS[semantic_type]["mapping"];
+    let fields = [];
+    const field_values = Object.values(mapping);
+    for (let index = 0; index < field_values.length; index++) {
+        fields = fields.concat(field_values[index]);
+    };
+    console.log('fields', fields)
+    return fields.sort().join(',')
+}
