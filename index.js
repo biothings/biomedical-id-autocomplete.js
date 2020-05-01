7bd22328f9834a139e8c2f67626bd6a68012387c
jiwen xin <kevinxin@scripps.edu>
2020-05-01 10:01:26 -0700

    add get display message method

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const ID_RESOLVING_APIS = require('./config').ID_RESOLVING_APIS;
 
 /**
  * Update the primary ID from id options
- * The return value is an axios post query promises
  * @param {string} semantic_type - The semantic type of IDs
  * @param {object} id_dict - An object of id representation
  * @returns - an id_dict updated with primary id choices
@@ -20,6 +19,23 @@ function get_primary_id(semantic_type, id_dict) {
             return res;
         }
     }
+}
+
+/**
+ * Update the display message from id options
+ * @param {string} semantic_type - The semantic type of IDs
+ * @param {object} id_dict - An object of id representation
+ * @returns - an id_dict updated with primary id choices
+ */
+exports.get_display_message = (semantic_type, id_dict) => {
+    let ranks = ID_RESOLVING_APIS[semantic_type]["id_ranks"];
+    let res = '';
+    for (let index = 0; index < ranks.length; index++) {
+        if (ranks[index] in id_dict) {
+            res += (ranks[index] + '(' + id_dict[ranks[index]] + ') ');
+        }
+    }
+    return res.replace(/^\s+|\s+$/g, '');
 }
 
 
