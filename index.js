3661c5c6beb4eafbb5f2c5fab0f881a4147838e1
jiwen xin <kevinxin@scripps.edu>
2020-05-01 09:42:18 -0700

    add primary id function

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
+const ID_RESOLVING_APIS = require('./config').ID_RESOLVING_APIS;
+
+/**
+ * Update the primary ID from id options
+ * The return value is an axios post query promises
+ * @param {string} semantic_type - The semantic type of IDs
+ * @param {object} id_dict - An object of id representation
+ * @returns - an id_dict updated with primary id choices
+ */
+function get_primary_id(semantic_type, id_dict) {
+    let ranks = ID_RESOLVING_APIS[semantic_type]["id_ranks"];
+    let res = {};
+    for (let index = 0; index < ranks.length; index++) {
+        if (ranks[index] in id_dict) {
+            res = {
+                identifier: ranks[index],
+                cls: semantic_type,
+                value: id_dict[ranks[index]]
+            };
+            return res;
+        }
+    }
+}
+
+
+exports.get_primary_id = get_primary_id;
